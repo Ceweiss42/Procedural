41d477aaef0fc80fd33a674d77a2f02703b6ed00
Ceweiss42 <42663413+Ceweiss42@users.noreply.github.com>
2020-05-27 18:05:37 -0700

    Add files via upload

---
+++ RayCastLegToTarget.cs
@@ -0,0 +1,65 @@
+﻿using JetBrains.Annotations;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RayCastLegToTarget : MonoBehaviour
+{
+    //this is the raycasted target
+    public GameObject legGrounded;
+
+    public RaycastHit hit;
+
+    //this is the name of the raycasted target, I was having problems with the GetComponent<GameObject>() method for some reason and I just bypassed it with the name.
+    public string name;
+
+    //the distance from leg to raycasted target
+    public double distance;
+
+    //tells the leg whene to move
+    public bool needToMove = false;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        legGrounded = GameObject.Find(name);
+        distance = 0;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //dray the Ray from leg to raycasted target, easy to see in editor
+        Debug.DrawRay(legGrounded.transform.position, (this.transform.position - legGrounded.transform.position), Color.red);
+
+        //3D pythagorean theroem to calculate distance from raycasted target to leg
+        double dX = legGrounded.transform.position.x - this.transform.position.x;
+        double dY = legGrounded.transform.position.y - this.transform.position.y;
+        double dZ = legGrounded.transform.position.z - this.transform.position.z;
+
+        distance = Math.Sqrt((dX * dX) + (dY * dY) + (dZ * dZ));
+
+        if(distance >= 1f)
+        {
+            Debug.Log("Need to move the leg");
+            needToMove = true;
+            
+        }
+
+        //the 7f is the speed of movement
+        if (needToMove)
+        {
+            //move the leg!!!!
+          this.transform.position = Vector3.MoveTowards(this.transform.position, legGrounded.transform.position, 7f * Time.deltaTime);
+        }
+
+        if(distance <= 0.05)
+        { //turn of the need to move
+            needToMove = false;
+        }
+
+    }
+}
---
+++ RayCastObjectDownward.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+public class RayCastObjectDownward : MonoBehaviour
+{
+
+    public RaycastHit hit;
+    public GameObject legTarget;
+    public string name;
+    
+
+
+    void Start()
+    { //this was explained in the other code file, but the GetComponent<GameObject>() method was not saving the objects for some reason
+        legTarget = GameObject.Find(name);
+       
+    }
+    void Update()
+    {
+        //draw ray from top down
+        Debug.DrawRay(transform.position, Vector3.down * 10f, Color.green);
+        
+        //if it hits the ground, move the target to the point where it hit
+        if (Physics.Raycast(transform.position, Vector3.down, out hit, 10f, 1))
+        {
+            legTarget.transform.position = hit.point;
+        }
+        else
+        {
+            Debug.Log("Nothing there");
+        }
+
+    }
+}
+
